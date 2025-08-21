2645b8d58c7e5a4f0c406ea102c3bbbf807da365
Milan Müller <development@milanmueller.de>
2025-08-21 19:36:45 +0200

    New datatypes

---
+++ src/Core.hs
@@ -0,0 +1,151 @@
+module Core where
+
+import Control.Monad.Except
+import Control.Monad.State
+import Data.HashSet as HashSet
+import Data.List (intercalate)
+import Data.Map as Map
+import Data.Time
+
+-- Mock State Representation --------------------------------------------------
+
+-- Metric Types --
+type MetricName = String
+
+data MetricType
+  = TypeInt
+  | TypeBool
+  | TypeEnum (HashSet String)
+  deriving (Eq)
+
+instance Show MetricType where
+  show TypeInt = "Integer number"
+  show TypeBool = "Boolean"
+  show (TypeEnum opts) = "Enum [" ++ intercalate ", " (HashSet.toList opts) ++ "]"
+
+-- Metric Values --
+data MetricValue = VInt Int | VBool Bool | VEnum String
+  deriving (Eq, Show)
+
+data DataPoint = DataPoint
+  { value :: MetricValue,
+    time :: UTCTime
+  }
+  deriving (Eq, Show)
+
+-- State -------------------------------------------------------------
+data DBState = DBState
+  { types :: Map MetricName MetricType,
+    values :: Map MetricName [DataPoint]
+  }
+  deriving (Show, Eq)
+
+-- Errors
+data DBError
+  = MetricNotFound MetricName
+  | TypeMismatch MetricName MetricType MetricValue
+  | InvalidEnumValue MetricName String (HashSet String)
+  | MetricAlreadyExists MetricName
+
+instance Show DBError where
+  show (MetricNotFound name) = "Metric '" ++ name ++ "' not found"
+  show (TypeMismatch name expected actual) =
+    "Type mismatch for metric '"
+      ++ name
+      ++ "': expected "
+      ++ show expected
+      ++ " but got "
+      ++ show actual
+  show (InvalidEnumValue name val validOpts) =
+    "Invalid enum value '"
+      ++ val
+      ++ "' for metric '"
+      ++ name
+      ++ "'. Valid options: "
+      ++ show (HashSet.toList validOpts)
+  show (MetricAlreadyExists name) = "Metric '" ++ name ++ "' already exists"
+
+type DBMonad = StateT DBState (Either DBError)
+
+emptyDBState :: DBState
+emptyDBState = DBState Map.empty Map.empty
+
+-- Operations
+addMetric :: MetricName -> MetricType -> DBMonad ()
+addMetric name mtype = do
+  s <- get
+  case Map.lookup name (types s) of
+    Just _ -> lift $ Left $ MetricAlreadyExists name
+    Nothing -> put $ s {types = Map.insert name mtype (types s)}
+
+getMetricType :: MetricName -> DBMonad MetricType
+getMetricType name = do
+  s <- get
+  case Map.lookup name (types s) of
+    Just mtype -> return mtype
+    Nothing -> lift $ Left $ MetricNotFound name
+
+validateValue :: MetricType -> MetricValue -> Either DBError ()
+validateValue TypeInt (VInt _) = Right ()
+validateValue TypeBool (VBool _) = Right ()
+validateValue (TypeEnum validOpts) (VEnum val) =
+  if HashSet.member val validOpts
+    then Right ()
+    else Left $ InvalidEnumValue "" val validOpts
+validateValue expected actual = Left $ TypeMismatch "" expected actual
+
+addDataPoint :: MetricName -> MetricValue -> UTCTime -> DBMonad ()
+addDataPoint name val timestamp = do
+  mtype <- getMetricType name
+  case validateValue mtype val of
+    Left (TypeMismatch _ expected actual) -> lift $ Left $ TypeMismatch name expected actual
+    Left (InvalidEnumValue _ val' opts) -> lift $ Left $ InvalidEnumValue name val' opts
+    Left err -> lift $ Left err
+    Right () -> do
+      s <- get
+      let oldPoints = Map.findWithDefault [] name (values s)
+          newPoint = DataPoint val timestamp
+          newPoints = newPoint : oldPoints
+      put $ s {values = Map.insert name newPoints (values s)}
+
+getMetricData :: MetricName -> DBMonad [DataPoint]
+getMetricData name = do
+  s <- get
+  case Map.lookup name (types s) of
+    Nothing -> lift $ Left $ MetricNotFound name
+    Just _ -> return $ Map.findWithDefault [] name (values s)
+
+deleteMetric :: MetricName -> DBMonad ()
+deleteMetric name = do
+  s <- get
+  case Map.lookup name (types s) of
+    Nothing -> lift $ Left $ MetricNotFound name
+    Just _ ->
+      put $
+        s
+          { types = Map.delete name (types s),
+            values = Map.delete name (values s)
+          }
+
+listMetrics :: DBMonad [(MetricName, MetricType)]
+listMetrics = do gets (Map.toList . types)
+
+mytest :: IO ()
+mytest = do
+  let initialState = emptyDBState
+
+  let combinedOps = do
+        addMetric "cpu" TypeInt
+        addMetric "mode" (TypeEnum $ HashSet.fromList ["user", "system", "idle"])
+        let t = read "2023-01-01 14:00:00 UTC"
+        addDataPoint "cpu" (VInt 85) t
+        addDataPoint "mode" (VEnum "user") t
+        listMetrics
+
+  case runStateT combinedOps initialState of
+    Left err -> putStrLn $ "Fehler: " ++ show err
+    Right (metrics, finalState) -> do
+      putStrLn "Definierte Metriken:"
+      mapM_ print metrics
+      putStrLn "\nFinaler State:"
+      print finalState
